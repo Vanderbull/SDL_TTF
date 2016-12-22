d5ecbe39c9cd1f8f35953ee371e14a15626c2d76
Me <bjonke@users.noreply.github.com>
2016-12-22 21:04:33 +0100

    Update ttf.cpp

--- ttf.cpp
+++ ttf.cpp
@@ -7,9 +7,39 @@ SDL_Rect shadedRect;
 bool SetupTTF( const std::string &fontName );
 SDL_Texture* SurfaceToTexture( SDL_Surface* surf );
 void CreateTextTextures();
+void Init();
 
 // Path for a TrueType font in ubuntu 16.04
 std::string = "/usr/share/fonts/truetype/freefont/FreeSans.ttf";
+
+int main()
+{
+	Init();
+	renderer = SDL_CreateRenderer(window, -1, 0);
+	SetupRenderer();
+	SetupTTF( "/usr/share/fonts/truetype/freefont/FreeSans.ttf" );
+	CreateTextTextures();
+        SDL_RenderCopy( renderer, solidTexture, nullptr, &solidRect );
+        SDL_RenderCopy( renderer, blendedTexture, nullptr, &blendedRect );
+	SDL_RenderCopy( renderer, shadedTexture, nullptr, &shadedRect );
+}
+
+void Init()
+{
+	// Initiliaze SDL video and our timer //
+	if( SDL_Init( SDL_INIT_VIDEO | SDL_INIT_TIMER | SDL_INIT_AUDIO) != 0 )
+	{
+        std::cout << "Unable to initialize SDL2: " << SDL_GetError() << std::endl;
+	};
+	window = SDL_CreateWindow(WINDOW_CAPTION,SDL_WINDOWPOS_UNDEFINED, 
+				  SDL_WINDOWPOS_UNDEFINED, WINDOW_WIDTH, WINDOW_HEIGHT, 0);
+	
+	// Adding some graphics for the icon of the application
+	SDL_Surface* surface2 = IMG_Load( "./atari.png" );
+	// The icon is attached to the window pointer
+    	SDL_SetWindowIcon(window, surface2);
+	SDL_FreeSurface(surface2);
+}
 
 bool SetupTTF( const std::string &fontName)
 {
